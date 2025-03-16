a74123dde2da02889d249b2931274c2962ee5848
Vextotem <iftmediainfo@gmail.com>
2025-03-16 22:57:14 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', (event) => {
     console.log('Service Worker installed');
     event.waitUntil(
-        caches.open('raket-tv-cache').then((cache) => {
+        caches.open('cinebyapp-cache').then((cache) => {
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/manifest.json',
-                'https://cdn.jsdelivr.net/gh/Vextotem/raketmeta@main/raket.png'
+                'https://cdn.jsdelivr.net/gh/Vextotem/cinebymeta@main/cineby.png'
             ]);
         })
     );
