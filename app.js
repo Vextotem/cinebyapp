644c07e88dd4955017d106d4fea56a9f8bcbd48d
Vextotem <iftmediainfo@gmail.com>
2025-03-16 22:59:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
-        .then((reg) => console.log('Service Worker registered:', reg))
-        .catch((err) => console.log('Service Worker registration failed:', err));
+        .then((reg) => console.log('CinebyApp Service Worker registered:', reg))
+        .catch((err) => console.log('CinebyApp Service Worker registration failed:', err));
 }
